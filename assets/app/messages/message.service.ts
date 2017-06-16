1310515ff5eeee3fd8e0415f2e0350545791597f
JN <jeetesh.n@gmail.com>
2017-06-16 16:04:10 +0530

    Changes for AWS delpoy

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -20,7 +20,7 @@ export class MessageService {
         const token = localStorage.getItem('token')
             ? '?token=' + localStorage.getItem('token')
             : '';
-        return this.http.post('https://ng-deploy-jn.herokuapp.com/message' + token, body, {headers: headers})
+        return this.http.post('http://ngdeployaws1-env.us-east-1.elasticbeanstalk.com/message' + token, body, {headers: headers})
             .map((response: Response) => {
                 const result = response.json();
                 const message = new Message(
@@ -38,7 +38,7 @@ export class MessageService {
     }
 
     getMessages() {
-        return this.http.get('https://ng-deploy-jn.herokuapp.com/message')
+        return this.http.get('http://ngdeployaws1-env.us-east-1.elasticbeanstalk.com/message')
             .map((response: Response) => {
                 const messages = response.json().obj;
                 let transformedMessages: Message[] = [];
@@ -69,7 +69,7 @@ export class MessageService {
         const token = localStorage.getItem('token')
             ? '?token=' + localStorage.getItem('token')
             : '';
-        return this.http.patch('https://ng-deploy-jn.herokuapp.com/message/' + message.messageId + token, body, {headers: headers})
+        return this.http.patch('http://ngdeployaws1-env.us-east-1.elasticbeanstalk.com/message/' + message.messageId + token, body, {headers: headers})
             .map((response: Response) => response.json())
             .catch((error: Response) => {
                 this.errorService.handleError(error.json());
@@ -82,7 +82,7 @@ export class MessageService {
         const token = localStorage.getItem('token')
             ? '?token=' + localStorage.getItem('token')
             : '';
-        return this.http.delete('https://ng-deploy-jn.herokuapp.com/message/' + message.messageId + token)
+        return this.http.delete('http://ngdeployaws1-env.us-east-1.elasticbeanstalk.com/message/' + message.messageId + token)
             .map((response: Response) => response.json())
             .catch((error: Response) => {
                 this.errorService.handleError(error.json());
